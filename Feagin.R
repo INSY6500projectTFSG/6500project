95066e3a9facdd0e8497ef026beb20b368c574ae
tbf0006 <55467476+tbf0006@users.noreply.github.com>
2019-12-02 16:04:18 -0600

    Create Feagin.R

---
+++ Feagin.R
@@ -0,0 +1,4 @@
+# Code modified from Jeff Smith's 'exploratory_data_anaysis.R'
+library("tidyverse")
+testdf <- read_csv("C:\\Users\\tbfea\\OneDrive\\Documents\\GitHub\\6500project\\UniqueAlbumFeat.csv")
+str(testdf)
