2072a776ef8cf0bb87e0594d771e15101636690f
tbf0006 <55467476+tbf0006@users.noreply.github.com>
2019-12-03 12:36:29 -0600

    Update Feagin.R

--- Feagin.R
+++ Feagin.R
@@ -1,4 +1,6 @@
 # Code modified from Jeff Smith's 'exploratory_data_anaysis.R'
 library("tidyverse")
-testdf <- read_csv("C:\\Users\\tbfea\\OneDrive\\Documents\\GitHub\\6500project\\UniqueAlbumFeat.csv")
+R1_feat <- read_csv("C:\\Users\\tbfea\\OneDrive\\Documents\\GitHub\\6500project\\UniqueAlbumFeat.csv")
 str(testdf)
+
+FreqR1 <- read_csv("C:\\Users\\tbfea\\OneDrive\\Documents\\GitHub\\6500project\\FreqOfNum1.csv")
