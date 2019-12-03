59aa53b1de32d437e762388abad1478a56bfdba6
tbf0006 <55467476+tbf0006@users.noreply.github.com>
2019-12-03 13:27:52 -0600

    Update Feagin.R

--- Feagin.R
+++ Feagin.R
@@ -13,47 +13,68 @@ ggplot(data=filter(FreqR1, id > 10)) +
 
 ggplot(data = R1_feat) +
   geom_point(mapping = aes(x = album, y = acousticness, color = id)) +
-  scale_color_gradient(low="blue", high="red")
+  scale_color_gradient(low="blue", high="red")+
+  geom_hline(yintercept=mean(R1_feat$acousticness), color="green")
 
 ggplot(data = R1_feat) +
   geom_point(mapping = aes(x = album, y = danceability, color = id)) +
-  scale_color_gradient(low="blue", high="red")
+  scale_color_gradient(low="blue", high="red") +
+  geom_hline(yintercept=mean(R1_feat$danceability), color="green")
+
 
 ggplot(data = R1_feat) +
   geom_point(mapping = aes(x = album, y = energy, color = id)) +
-  scale_color_gradient(low="blue", high="red")
+  scale_color_gradient(low="blue", high="red") +
+  geom_hline(yintercept=mean(R1_feat$energy), color="green")
+
 
 ggplot(data = R1_feat) +
   geom_point(mapping = aes(x = album, y = instrumentalness, color = id)) +
-  scale_color_gradient(low="blue", high="red")
+  scale_color_gradient(low="blue", high="red") +
+  geom_hline(yintercept=mean(R1_feat$instrumentalness), color="green")
+
 
 ggplot(data = R1_feat) +
   geom_point(mapping = aes(x = album, y = key, color = id)) +
-  scale_color_gradient(low="blue", high="red")
+  scale_color_gradient(low="blue", high="red") +
+  geom_hline(yintercept=mean(R1_feat$key), color="green")
+
 
 ggplot(data = R1_feat) +
   geom_point(mapping = aes(x = album, y = liveness, color = id)) +
-  scale_color_gradient(low="blue", high="red")
+  scale_color_gradient(low="blue", high="red") +
+  geom_hline(yintercept=mean(R1_feat$liveness), color="green")
+
 
 ggplot(data = R1_feat) +
   geom_point(mapping = aes(x = album, y = loudness, color = id)) +
-  scale_color_gradient(low="blue", high="red")
+  scale_color_gradient(low="blue", high="red") +
+  geom_hline(yintercept=mean(R1_feat$loudness), color="green")
+
 
 ggplot(data = R1_feat) +
   geom_point(mapping = aes(x = album, y = mode, color = id)) +
-  scale_color_gradient(low="blue", high="red")
+  scale_color_gradient(low="blue", high="red") +
+  geom_hline(yintercept=mean(R1_feat$mode), color="green")
+
 
 ggplot(data = R1_feat) +
   geom_point(mapping = aes(x = album, y = speechiness, color = id)) +
-  scale_color_gradient(low="blue", high="red")
+  scale_color_gradient(low="blue", high="red") +
+  geom_hline(yintercept=mean(R1_feat$speechiness), color="green")
+
 
 ggplot(data = R1_feat) +
   geom_point(mapping = aes(x = album, y = tempo, color = id)) +
-  scale_color_gradient(low="blue", high="red")
+  scale_color_gradient(low="blue", high="red") +
+  geom_hline(yintercept=mean(R1_feat$tempo), color="green")
+
 
 ggplot(data = R1_feat) +
   geom_point(mapping = aes(x = album, y = valence, color = id)) +
-  scale_color_gradient(low="blue", high="red")
+  scale_color_gradient(low="blue", high="red") +
+  geom_hline(yintercept=mean(R1_feat$valence), color="green")
+
 
 ggplot(data=filter(R1_feat, id > 10)) +
   geom_point(mapping = aes(x = album, y = acousticness, color = id)) +
